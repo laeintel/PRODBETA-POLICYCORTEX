5813a957a5b18c5960f8924fdee950d07de53f94
Leonard Esere <leonard.esere@aeolitech.com>
2025-08-14 12:34:05 -0500

    terraform: parameterize state backend creation to avoid 409 conflicts; fix OpenAI deployment scale blocks; validate ok

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -2,16 +2,24 @@
 # This file now only contains the storage account resources for state management
 
 # Resource group to hold Terraform state resources
+variable "manage_state_storage" {
+  description = "Whether to create/own the Terraform state storage resources"
+  type        = bool
+  default     = false
+}
+
 resource "azurerm_resource_group" "terraform_state" {
+  count    = var.manage_state_storage ? 1 : 0
   name     = "rg-terraform-state-${var.environment}"
   location = var.location
 }
 
 # Create storage account for Terraform state with locking enabled
 resource "azurerm_storage_account" "terraform_state" {
+  count                   = var.manage_state_storage ? 1 : 0
   name                     = "stterraformstate${var.environment}"
-  resource_group_name      = azurerm_resource_group.terraform_state.name
-  location                 = azurerm_resource_group.terraform_state.location
+  resource_group_name      = azurerm_resource_group.terraform_state[0].name
+  location                 = azurerm_resource_group.terraform_state[0].location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
@@ -36,13 +44,15 @@ resource "azurerm_storage_account" "terraform_state" {
 }
 
 resource "azurerm_storage_container" "terraform_state" {
+  count                 = var.manage_state_storage ? 1 : 0
   name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.terraform_state.name
+  storage_account_name  = azurerm_storage_account.terraform_state[0].name
   container_access_type = "private"
 }
 
 # Lock table for additional safety (optional with Azure)
 resource "azurerm_storage_table" "terraform_locks" {
+  count                = var.manage_state_storage ? 1 : 0
   name                 = "terraformlocks"
-  storage_account_name = azurerm_storage_account.terraform_state.name
+  storage_account_name = azurerm_storage_account.terraform_state[0].name
 }
